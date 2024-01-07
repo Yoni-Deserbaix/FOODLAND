e0a93c841ba001caa9f9aa1db67517dd9752eb0d
unknown <yonideserbaix@gmail.com>
2024-01-07 16:58:58 +0100

    commit 8

--- script.js
+++ script.js
@@ -6,3 +6,48 @@ let swiper = new Swiper(".home", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+const sections = document.querySelectorAll("section");
+const header = document.querySelector("header");
+const btnBurger = document.querySelector("#burger-menu");
+const nav = document.querySelector(".navigation");
+const linkNav = document.querySelectorAll(".navigation a");
+
+btnBurger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  btnBurger.classList.toggle("bx-x");
+});
+
+linkNav.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+    btnBurger.classList.remove("bx-x");
+  });
+});
+
+window.addEventListener("scroll", () => {
+  nav.classList.remove("active");
+  btnBurger.classList.remove("bx-x");
+});
+
+const scrollActive = () => {
+  sections.forEach((section) => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 150;
+    let height = section.offsetHeight;
+    let id = section.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      linkNav.forEach((links) => {
+        links.classList.remove("active");
+        document.querySelector(`.navigation a[href*=${id}]`).classList.add("active");
+      });
+    }
+  });
+};
+
+window.addEventListener("scroll", () => {
+  scrollActive();
+  console.log("Scrolled"); // Vérification scroll
+});
+
